3346c9f00097a11ed0e5a70f7ac52bd544ce9a27
oddtyme <seamus@live.unc.edu>
2019-10-20 23:23:47 -0400

    Set up basic quiz infrastructure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+
+const test_question = 'Are you a utilitarian?';
+
+class Question extends React.Component {
+	render() {
+		return (
+			<div className='question_answer_container'>
+				<div className='question'>
+					{test_question} 
+				</div>
+			
+				<div className='answers'>
+					<button
+				 	className='yes-util'
+                 	onClick={() => this.setState({util: 1})}
+  					>
+						Yes
+					</button>
+
+					<button
+					className='no-util'
+					onClick={() => this.setState({util: 0})}
+					>
+						No
+					</button>
+				</div>
+			</div>
+		);
+	}
+}
+			
+
+
+class Questions extends React.Component {
+	renderQuestion() {
+    	return <Question />;
+	}	
+
+	render() {
+	 return (<div>
+		{this.renderQuestion()}
+	 </div>);
+	}
+}
+		
+class App extends React.Component {
+	render() {
+		return (
+			<div className="app">
+				<div className="questions_container">
+					<Questions />
+				</div>
+			</div>
+		);
+	}
+}
+
 
 ReactDOM.render(
   <App />,
